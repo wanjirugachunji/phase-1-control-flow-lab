ba7dd21e17d2335ec040850a4128c0c49c5f264a
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-06 23:50:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function scuberGreetingForFeet(feet){
+    // Write your code here!
+    if (feet <= 400){
+      return 'This one is on me!'
+    }
+    else if (feet>2500){
+      return 'No can do.'
+    }
+  else if (feet >2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+  }
+  function ternaryCheckCity(city){
+    // Write your code here!
+    {return city === 'NYC' ?  "Ok, sounds good.": 'No go.'}
+  
+  }
+  
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+    if (tip=== 'generous'){
+      return 'Thank you so much.'
+    }
+    else if (tip === 'not as generous'){
+      return 'Thank you.'
+    }
+    else{
+    return 'Bye.'
+    }
+  } 
